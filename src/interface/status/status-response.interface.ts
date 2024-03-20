52a94f805ea0d56f274abcd859f63df2d002274b
Renato HMS <trowdev@gmail.com>
2024-03-20 18:29:27 -0300

    Add novas propriedades interface de status pedido

--- src/interface/status/status-response.interface.ts
+++ src/interface/status/status-response.interface.ts
@@ -1,5 +1,11 @@
 export interface StatusResponseInterface {
-    authorizationId: string;
-    referenceId: string;
-    status: string;
+    referencia: string;
+    uuid: string;
+    observacao: string;
+    pixCopiaCola: string;
+    valor: number;
+    taxa: number;
+    status: 'CONCLUIDO' | 'APROVADO' | 'ANDAMENTO' | 'PENDENTE' | 'ESGOTADO' | 'ANALISE' | 'REJEITADO';
+    tipoOperacao: 'C' | 'D';
+    tipoReferencia: 'INTERNO' | 'EXTERNO';
 }
